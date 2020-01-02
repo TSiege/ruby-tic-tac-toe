96c1cffbf80ead1c6a23cb51a1010dc4aac30edf
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-01-02 14:50:19 -0500

    more board features

--- board.rb
+++ board.rb
@@ -1,84 +1,91 @@
+BLANK = " "
+
 TILE_TO_POSITION = {
-    A1: [0, 0],
-    A2: [1, 0],
-    A3: [2, 0],
-    B1: [0, 1],
-    B2: [1, 1],
-    B3: [2, 1],
-    C1: [0, 2],
-    C2: [1, 2],
-    C3: [2, 2],
-    "1A": [0, 0],
-    "2A": [1, 0],
-    "3A": [2, 0],
-    "1B": [0, 1],
-    "2B": [1, 1],
-    "3B": [2, 1],
-    "1C": [0, 2],
-    "2C": [1, 2],
-    "3C": [2, 2],
+  A1: [0, 0],
+  A2: [1, 0],
+  A3: [2, 0],
+  B1: [0, 1],
+  B2: [1, 1],
+  B3: [2, 1],
+  C1: [0, 2],
+  C2: [1, 2],
+  C3: [2, 2],
+  "1A": [0, 0],
+  "2A": [1, 0],
+  "3A": [2, 0],
+  "1B": [0, 1],
+  "2B": [1, 1],
+  "3B": [2, 1],
+  "1C": [0, 2],
+  "2C": [1, 2],
+  "3C": [2, 2],
 }
 
 class Board
-    attr_accessor :board
+  attr_accessor :board
 
-    def initialize
-        self.reset
+  def initialize(board = [[BLANK, BLANK, BLANK], [BLANK, BLANK, BLANK], [BLANK, BLANK, BLANK]])
+    @board = board
+  end
+
+  def reset
+    @board = [[BLANK, BLANK, BLANK], [BLANK, BLANK, BLANK], [BLANK, BLANK, BLANK]]
+  end
+
+  def print
+    puts ["  A   B   C", *board.map.with_index { |r, i| "#{i + 1} #{r.join(" | ")}" }].join("\n")
+  end
+
+  def take_space(row, col, marker)
+    raise "Invalid marker: #{marker}" if marker != "X" && marker != "O"
+    board[row][col] = marker
+    board
+  end
+
+  def take_space_via_tile(tile, marker)
+    position = TILE_TO_POSITION[tile.upcase.to_sym]
+    raise "Invalid tile: #{tile}" if position.nil?
+    take_space(*position, marker)
+  end
+
+  def is_space_free?(row, col)
+    board[row][col] == BLANK
+  end
+
+  def is_board_full?
+    board.flatten.none? { |x| x == BLANK }
+  end
+
+  def free_space_count
+    9 - board.flatten.filter { |x| x != BLANK }.length
+  end
+
+  def has_winner?
+    !!find_winner
+  end
+
+  def find_winner
+    # horizontal winners
+    board.each do |row|
+      return row.first if find_winner_for_row row
     end
-
-    def reset
-        @board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    # vertical winners
+    [0, 1, 2].each do |col|
+      row = [board[0][col], board[1][col], board[2][col]]
+      return row.first if find_winner_for_row row
     end
+    # diagonal winners
+    backward_diag = [board[0][0], board[1][1], board[2][2]]
+    forward_diag = [board[0][2], board[1][1], board[2][0]]
+    return backward_diag.first if find_winner_for_row backward_diag
+    return backward_diag.first if find_winner_for_row forward_diag
 
-    def print
-        puts ["  A   B   C", *board.map.with_index { |r, i| "#{i + 1} #{r.join(" | ")}" }].join("\n")
-    end
+    return nil
+  end
 
-    def take_space(row, col, marker)
-        raise "Invalid marker: #{marker}" if marker != "X" && marker != "O"
-        board[row][col] = marker
-    end
-
-    def take_space_via_tile(tile, marker)
-        position = TILE_TO_POSITION[tile.upcase.to_sym]
-        raise "Invalid tile: #{tile}" if position.nil?
-        take_space(*position, marker)
-    end
-
-    def is_space_free?(row, col)
-        board[row][col] == " "
-    end
-
-    def is_board_full?
-        board.flatten.none? { |x| x == " " }
-    end
-
-    def has_winner?
-        !!find_winner
-    end
-
-    def find_winner
-        # horizontal winners
-        board.each do |row|
-            return row.first if find_winner_for_row row
-        end
-        # vertical winners
-        [0, 1, 2].each do |col|
-            row = [board[0][col], board[1][col], board[2][col]]
-            return row.first if find_winner_for_row row
-        end
-        # diagonal winners
-        backward_diag = [board[0][0], board[1][1], board[2][2]]
-        forward_diag = [board[0][2], board[1][1], board[2][0]]
-        return backward_diag.first if find_winner_for_row backward_diag
-        return backward_diag.first if find_winner_for_row forward_diag
-
-        return nil
-    end
-
-    private def find_winner_for_row(row)
-        return row.first if row.uniq.length == 1 && row.none?(" ")
-        return nil
-    end
+  private def find_winner_for_row(row)
+    return row.first if row.uniq.length == 1 && row.none?(BLANK)
+    return nil
+  end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,63 +1,68 @@
 require 'spec_helper'
 
 describe Board do
-    it "is created with an empty board" do
-        expect(described_class.new.board).to match_array([[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]])
-    end
+  it "is created with an empty board" do
+    expect(Board.new.board).to match_array([[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]])
+  end
 
-    it "can take a move" do
-        board = described_class.new
-        board.take_space(0, 0, "X")
-        expect(board.board).to match_array([["X", " ", " "], [" ", " ", " "], [" ", " ", " "]])
-    end
+  it "can take a move" do
+    board = Board.new
+    board.take_space(0, 0, "X")
+    expect(board.board).to match_array([["X", " ", " "], [" ", " ", " "], [" ", " ", " "]])
+  end
 
-    it "can take a move via a tile" do
-        board = described_class.new
-        board.take_space_via_tile("A1", "X")
-        expect(board.board).to match_array([["X", " ", " "], [" ", " ", " "], [" ", " ", " "]])
-    end
+  it "can take a move via a tile" do
+    board = Board.new
+    board.take_space_via_tile("A1", "X")
+    expect(board.board).to match_array([["X", " ", " "], [" ", " ", " "], [" ", " ", " "]])
+  end
 
-    it "can check if a space is free" do
-        board = described_class.new
-        board.take_space_via_tile("A1", "X")
-        expect(board.is_space_free?(0, 0, )).to equal false
-        expect(board.is_space_free?(0, 1, )).to equal true
-    end
+  it "can check if a space is free" do
+    board = Board.new
+    board.take_space_via_tile("A1", "X")
+    expect(board.is_space_free?(0, 0, )).to equal false
+    expect(board.is_space_free?(0, 1, )).to equal true
+  end
 
-    it "can check if a board is full" do
-        board = described_class.new
-        board.board = [[" ", "X", "O"], ["O", "X", "X"], ["X", "O", "O"]]
-        expect(board.is_board_full?).to equal false
-        board.take_space(0, 0, "X")
-        expect(board.is_board_full?).to equal true
-    end
+  it "can check if a board is full" do
+    board = Board.new([[" ", "X", "O"], ["O", "X", "X"], ["X", "O", "O"]])
+    expect(board.is_board_full?).to equal false
+    board.take_space(0, 0, "X")
+    expect(board.is_board_full?).to equal true
+  end
 
+  it "knows how many spaces are blank" do
+    board = Board.new
+    expect(board.free_space_count).to equal 9
+    board.board = [[" ", "X", "O"], ["O", "X", "X"], ["X", "O", "O"]]
+    expect(board.free_space_count).to equal 1
+    board.board = [["X", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    expect(board.free_space_count).to equal 8
+  end
 
-    it "can check if a board has a winner" do
-        board = described_class.new
-        board.board = [[" ", "X", "O"], ["O", "X", "X"], ["X", "O", "O"]]
-        expect(board.has_winner?).to equal false
-        board.take_space(0, 0, "X")
-        expect(board.has_winner?).to equal false
-        board.board = [["X", "X", "X"], [" ", " ", " "], [" ", " ", " "]]
-        expect(board.has_winner?).to equal true
-        board.board = [["O", " ", " "], ["O", " ", " "], ["O", " ", " "]]
-        expect(board.has_winner?).to equal true
-        board.board = [["O", " ", " "], [" ", "O", " "], [" ", " ", "O"]]
-        expect(board.has_winner?).to equal true
-    end
+  it "can check if a board has a winner" do
+    board = Board.new([[" ", "X", "O"], ["O", "X", "X"], ["X", "O", "O"]])
+    expect(board.has_winner?).to equal false
+    board.take_space(0, 0, "X")
+    expect(board.has_winner?).to equal false
+    board.board = [["X", "X", "X"], [" ", " ", " "], [" ", " ", " "]]
+    expect(board.has_winner?).to equal true
+    board.board = [["O", " ", " "], ["O", " ", " "], ["O", " ", " "]]
+    expect(board.has_winner?).to equal true
+    board.board = [["O", " ", " "], [" ", "O", " "], [" ", " ", "O"]]
+    expect(board.has_winner?).to equal true
+  end
 
-    it "can find the winner" do
-        board = described_class.new
-        board.board = [[" ", "X", "O"], ["O", "X", "X"], ["X", "O", "O"]]
-        expect(board.find_winner).to equal nil
-        board.take_space(0, 0, "X")
-        expect(board.find_winner).to equal nil
-        board.board = [["X", "X", "X"], [" ", " ", " "], [" ", " ", " "]]
-        expect(board.find_winner).to eql "X"
-        board.board = [["O", " ", " "], ["O", " ", " "], ["O", " ", " "]]
-        expect(board.find_winner).to eql "O"
-        board.board = [["O", " ", " "], [" ", "O", " "], [" ", " ", "O"]]
-        expect(board.find_winner).to eql "O"
-    end
+  it "can find the winner" do
+    board = Board.new([[" ", "X", "O"], ["O", "X", "X"], ["X", "O", "O"]])
+    expect(board.find_winner).to equal nil
+    board.take_space(0, 0, "X")
+    expect(board.find_winner).to equal nil
+    board.board = [["X", "X", "X"], [" ", " ", " "], [" ", " ", " "]]
+    expect(board.find_winner).to eql "X"
+    board.board = [["O", " ", " "], ["O", " ", " "], ["O", " ", " "]]
+    expect(board.find_winner).to eql "O"
+    board.board = [["O", " ", " "], [" ", "O", " "], [" ", " ", "O"]]
+    expect(board.find_winner).to eql "O"
+  end
 end
